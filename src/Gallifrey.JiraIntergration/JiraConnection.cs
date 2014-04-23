a1c73f3c562588129573b65ddb95ba2f0dc0fe8b
Chris Blyth <chris.blyth@15below.com>
2014-04-23 14:11:01 +0100

    Start of #3 adding Jira Layer in.

--- src/Gallifrey.JiraIntergration/JiraConnection.cs
+++ src/Gallifrey.JiraIntergration/JiraConnection.cs
@@ -1,14 +1,17 @@
-﻿using Gallifrey.Settings;
+﻿using Atlassian.Jira;
+using Gallifrey.Settings;
 
 namespace Gallifrey.JiraIntergration
 {
     public class JiraConnection
     {
         private readonly AppSettings appSettings;
+        private readonly Jira jira;
 
         public JiraConnection(AppSettings appSettings)
         {
             this.appSettings = appSettings;
+            jira = new Jira(appSettings.JiraUrl, appSettings.JiraUsername, appSettings.JiraPassword);
         }
     }
 }
