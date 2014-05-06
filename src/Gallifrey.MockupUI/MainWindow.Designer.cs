eee86df6acbad2bc65f5ad26449568bb97f9ee58
Chris Blyth <chris.blyth@15below.com>
2014-05-06 15:15:00 +0100

    Feature #26 - Added auto updating version number

--- src/Gallifrey.MockupUI/MainWindow.Designer.cs
+++ src/Gallifrey.MockupUI/MainWindow.Designer.cs
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.btnRemoveTimer = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -45,27 +45,27 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(88, 12);
+            this.label1.Location = new System.Drawing.Point(92, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(163, 50);
             this.label1.TabIndex = 1;
             this.label1.Text = "Gallifrey";
             // 
-            // label2
+            // lblVersion
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(287, 38);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 17);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Version: 0.0.0.0";
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVersion.Location = new System.Drawing.Point(98, 79);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(77, 13);
+            this.lblVersion.TabIndex = 3;
+            this.lblVersion.Text = "v0.0.0.0 (beta)";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(94, 62);
+            this.label3.Location = new System.Drawing.Point(98, 59);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(183, 17);
             this.label3.TabIndex = 4;
@@ -128,7 +128,7 @@
             this.Controls.Add(this.tabTimerDays);
             this.Controls.Add(this.btnRemoveTimer);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblVersion);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAddTimer);
@@ -149,7 +149,7 @@
         private System.Windows.Forms.Button btnAddTimer;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblVersion;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnRemoveTimer;
         private System.Windows.Forms.TabControl tabTimerDays;
