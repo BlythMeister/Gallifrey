50c387f31a75d2e81906d3873b5b57a34dfd4fc3
Chris Blyth <chris.blyth@15below.com>
2014-05-07 12:40:36 +0100

    Features for #27 adding in AppSettings

--- src/Gallifrey/Settings/AppSettings.cs
+++ src/Gallifrey/Settings/AppSettings.cs
@@ -4,6 +4,13 @@ namespace Gallifrey.Settings
 {
     public class AppSettings
     {
+        public bool AlertWhenNotRunning { get; set; }
+        public int AlertTimeMins { get; set; }
+        public int KeepTimersForDays { get; set; }
+        public int TargetLogHoursPerDay { get; set; }
+        public bool UiAlwaysOnTop { get; set; }
+        public UiAnimationLevel UiAnimationLevel { get; set; }
+
         public void SaveSettings()
         {
             AppSettingsSerializer.Serialize(this);    
