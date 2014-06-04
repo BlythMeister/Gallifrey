91989eb521cecdf9ad44a8643c694628ec03c6e3
Chris Blyth <chris.blyth@15below.com>
2014-06-04 15:12:24 +0100

    Correct issue with Idle timers disappearing

--- src/Gallifrey/IdleTimers/IdleTimerCollection.cs
+++ src/Gallifrey/IdleTimers/IdleTimerCollection.cs
@@ -66,7 +66,7 @@ namespace Gallifrey.IdleTimers
 
         public void RemoveOldTimers()
         {
-            lockTimerList.RemoveAll(idleTimer => !idleTimer.IsRunning && idleTimer.DateStarted >= DateTime.Now.AddDays(-1));
+            lockTimerList.RemoveAll(idleTimer => !idleTimer.IsRunning && idleTimer.DateStarted <= DateTime.Now.AddDays(-1));
             SaveTimers();
         }
 
