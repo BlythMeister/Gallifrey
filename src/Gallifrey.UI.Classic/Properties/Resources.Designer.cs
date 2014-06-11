249658a5c2635692539e4ccc200e85a7c8278308
Chris Blyth <chris.blyth@15below.com>
2014-06-11 10:33:32 +0100

    Better Handling of timers creating new versions overnight

--- src/Gallifrey.UI.Classic/Properties/Resources.Designer.cs
+++ src/Gallifrey.UI.Classic/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Gallifrey.UI.Classic.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Calendar_48x48 {
+            get {
+                object obj = ResourceManager.GetObject("Calendar_48x48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Cancel_48x48 {
             get {
                 object obj = ResourceManager.GetObject("Cancel_48x48", resourceCulture);
