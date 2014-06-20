780687843610af644abcbf0db178a9b46fe9c315
Chris Blyth <chris.blyth@15below.com>
2014-06-20 14:44:11 +0100

    Add Comparer to sort jiras correctly with numerical parts

---
+++ src/Gallifrey/Comparers/JiraReferenceComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Gallifrey.Comparers
+{
+    public class JiraReferenceComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            var xParts = x.Split('-');
+            var yParts = y.Split('-');
+
+            return xParts[0] == yParts[0]
+                       ? int.Parse(xParts[1]).CompareTo(int.Parse(yParts[1]))
+                       : x.CompareTo(y);
+        }
+    }
+}
