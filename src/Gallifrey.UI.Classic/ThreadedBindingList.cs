afd1dcde85c1693a61a714fbb1a5169a161da0f0
Chris Blyth <chris.blyth@15below.com>
2014-06-23 12:02:37 +0100

    Refactor and change the way the listbox & tab page title is updated.
    Fix for #73.

---
+++ src/Gallifrey.UI.Classic/ThreadedBindingList.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Threading;
+
+namespace Gallifrey.UI.Classic
+{
+    public class ThreadedBindingList<T> : BindingList<T>
+    {
+        SynchronizationContext ctx = SynchronizationContext.Current;
+        protected override void OnAddingNew(AddingNewEventArgs e)
+        {
+            if (ctx == null)
+            {
+                BaseAddingNew(e);
+            }
+            else
+            {
+                ctx.Send(delegate
+                { BaseAddingNew(e); }, null);
+            }
+        }
+
+        void BaseAddingNew(AddingNewEventArgs e)
+        {
+            base.OnAddingNew(e);
+        }
+
+        protected override void OnListChanged(ListChangedEventArgs e)
+        {
+            if (ctx == null)
+            {
+                BaseListChanged(e);
+            }
+            else
+            {
+                ctx.Send(delegate
+                { BaseListChanged(e); }, null);
+            }
+        }
+
+        void BaseListChanged(ListChangedEventArgs e)
+        {
+            base.OnListChanged(e);
+        }
+    }
+}
