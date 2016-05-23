5bf9dec878ab3a15d7e09fb455ef340251e6d882
Chris Blyth <chris.blyth@15below.com>
2016-05-23 16:16:02 +0100

    Whoops, shouldn't have committed it like that!

--- src/Gallifrey.UI.Modern/MainViews/MainWindow.xaml.cs
+++ src/Gallifrey.UI.Modern/MainViews/MainWindow.xaml.cs
@@ -186,7 +186,7 @@ namespace Gallifrey.UI.Modern.MainViews
                         var idleTimer = modelHelpers.Gallifrey.IdleTimerCollection.GetTimer(idleTimerId);
                         if (idleTimer == null) return;
 
-                        if (idleTimer.IdleTimeValue.TotalSeconds < 0 || idleTimer.IdleTimeValue.TotalHours > 10)
+                        if (idleTimer.IdleTimeValue.TotalSeconds < 60 || idleTimer.IdleTimeValue.TotalHours > 10)
                         {
                             modelHelpers.Gallifrey.IdleTimerCollection.RemoveTimer(idleTimerId);
                         }
