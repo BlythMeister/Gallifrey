d16fba27d82ba45aa9f42d0433bc33e45e45f54e
Chris Blyth <blythmeister@gmail.com>
2017-09-20 13:58:04 +0100

    Version updating

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 ECHO OFF
 cls
-REM .paket\paket.bootstrapper.exe --run restore
+.paket\paket.bootstrapper.exe --run restore
 packages\FAKE\tools\FAKE.exe build.fsx
--- build.fsx
+++ build.fsx
@@ -9,19 +9,28 @@ open System.IO
 let outputDir = currentDirectory @@ "Output"
 let srcDir = currentDirectory @@ "src"
 let isAppVeyor = buildServer = AppVeyor
+let changeLogPath = currentDirectory @@ "src" @@ "Gallifrey.UI.Modern" @@ "ChangeLog.xml"
+let branchName = getBranchName currentDirectory
 
 Target "Clean" (fun _ -> 
     CleanDir outputDir
 )
 
 Target "VersionUpdate" (fun _ ->
-    let baseVersion = File.ReadAllText (srcDir @@ "CurrentVersion.info")
-    let versionNumber = match getBranchName currentDirectory, isAppVeyor with
-                        | _, false -> "0.0.0.0"
-                        | "master", _ -> sprintf "%s.0" baseVersion                        
-                        | _, _ -> sprintf "%s.%s" baseVersion AppVeyorEnvironment.BuildNumber
+    let baseVersion = match isAppVeyor with
+                      | true -> AppVeyorEnvironment.BuildVersion.Substring(0, AppVeyorEnvironment.BuildVersion.LastIndexOf("."))
+                      | _ -> "3.6.0"
+
+    let buildNumber = match isAppVeyor with
+                      | true -> AppVeyorEnvironment.BuildNumber
+                      | _ -> "6"
+
+    let versionNumber = match branchName with
+                        | "master" -> sprintf "%s.0" baseVersion                        
+                        | _ -> sprintf "%s.%s" baseVersion buildNumber
 
     BulkReplaceAssemblyInfoVersions "src/" (fun f -> { f with AssemblyVersion = versionNumber; AssemblyInformationalVersion = versionNumber; AssemblyFileVersion = versionNumber })
+    File.WriteAllText(changeLogPath, File.ReadAllText(changeLogPath).Replace("<Version Number=\"0.0.0.0\" Name=\"Pre-Release\">", (sprintf "<Version Number=\"%s\" Name=\"Pre-Release\">" versionNumber)))
 )
 
 Target "Build" DoNothing
