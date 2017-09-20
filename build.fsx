e38d826b51b7b321f7b98606c363609d32025b1c
Chris Blyth <blythmeister@gmail.com>
2017-09-20 15:00:15 +0100

    Build script update

--- build.fsx
+++ build.fsx
@@ -11,23 +11,27 @@ let srcDir = currentDirectory @@ "src"
 let isAppVeyor = buildServer = AppVeyor
 let changeLogPath = currentDirectory @@ "src" @@ "Gallifrey.UI.Modern" @@ "ChangeLog.xml"
 let branchName = getBranchName currentDirectory
+let isStable = branchName = "master"
+let isBeta = isStable || branchName = "develop"
+
+let baseVersion = match isAppVeyor with
+                  | true -> AppVeyorEnvironment.BuildVersion.Substring(0, AppVeyorEnvironment.BuildVersion.LastIndexOf("."))
+                  | _ -> "0.0.0"
+
+let buildNumber = match isAppVeyor with
+                  | true -> AppVeyorEnvironment.BuildNumber
+                  | _ -> "0"
+
+let versionNumber = match isStable with
+                    | true -> sprintf "%s.0" baseVersion                        
+                    | _ -> sprintf "%s.%s" baseVersion buildNumber
 
 Target "Clean" (fun _ -> 
     CleanDir outputDir
 )
 
 Target "VersionUpdate" (fun _ ->
-    let baseVersion = match isAppVeyor with
-                      | true -> AppVeyorEnvironment.BuildVersion.Substring(0, AppVeyorEnvironment.BuildVersion.LastIndexOf("."))
-                      | _ -> "0.0.0"
 
-    let buildNumber = match isAppVeyor with
-                      | true -> AppVeyorEnvironment.BuildNumber
-                      | _ -> "0"
-
-    let versionNumber = match branchName with
-                        | "master" -> sprintf "%s.0" baseVersion                        
-                        | _ -> sprintf "%s.%s" baseVersion buildNumber
 
     BulkReplaceAssemblyInfoVersions "src/" (fun f -> { f with AssemblyVersion = versionNumber; AssemblyInformationalVersion = versionNumber; AssemblyFileVersion = versionNumber })
 
@@ -40,10 +44,68 @@ Target "VersionUpdate" (fun _ ->
                                                                      .Replace("<ApplicationVersion>0.0.0.0</ApplicationVersion>", (sprintf "<ApplicationVersion>%s</ApplicationVersion>" versionNumber))))
 )
 
-Target "Build" DoNothing
-Target "Test" DoNothing
-Target "Package" DoNothing
-Target "Publish" DoNothing
+Target "Build" (fun _ ->
+    let buildMode = getBuildParamOrDefault "buildMode" "Release"
+    let setParams defaults = { defaults with Verbosity = Some(Quiet)
+                                             Targets = ["Clean,Rebuild,publish"]
+                                             Properties = [
+                                                            "Optimize", "True"
+                                                            "DebugSymbols", "True"
+                                                            "Configuration", buildMode
+                                                          ]
+                             }
+
+    build setParams (srcDir @@ "Gallifrey.sln")
+)
+
+Target "Test" (fun _ ->
+    printfn "There are no tests to run yet...."
+)
+
+Target "Package" (fun _ ->
+    let moveArtifacts releaseType = 
+        let source = srcDir @@ (sprintf "Gallifrey.UI.Modern.%s" releaseType) @@ "bin" @@ "Release" @@ "app.publish"
+        let destination = outputDir @@ releaseType
+        Directory.Move(source, destination)
+
+    moveArtifacts "Alpha"
+    moveArtifacts "Beta"
+    moveArtifacts "Stable"
+
+    File.Copy(outputDir @@ "Stable" @@ "setup.exe", outputDir @@ "stable-setup.exe")
+    File.Copy(outputDir @@ "Beta" @@ "setup.exe", outputDir @@ "beta-setup.exe")
+)
+
+Target "Publish" (fun _ ->
+    let publishRelease (releaseType:string) = 
+        let sourceRoot = outputDir @@ releaseType
+        let destinationRoot = currentDirectory @@ "Releases" @@ "download" @@ "modern-temp" @@ (releaseType.ToLower())
+        ensureDirectory destinationRoot
+        File.Copy(sourceRoot @@ (sprintf "Gallifrey.UI.Modern.%s.application" releaseType), destinationRoot @@ (sprintf "Gallifrey.UI.Modern.%s.application" releaseType), true)
+
+        let destinationFiles = destinationRoot @@ "Application Files"
+        ensureDirectory destinationFiles
+        Directory.GetDirectories(sourceRoot @@ "Application Files")
+        |> Seq.map(fun x -> new DirectoryInfo(x))
+        |> Seq.iter(fun x -> Directory.Move(x.FullName, destinationFiles @@ x.Name))
+    
+    let releasesRepo = outputDir @@ "Releases"
+    DeleteDir releasesRepo |> ignore
+    cloneSingleBranch outputDir "git@github.com:BlythMeister/Gallifrey.Releases.git" "master" "Releases"
+
+    publishRelease "Alpha"
+    if isBeta then publishRelease "Beta"
+    if isStable then publishRelease "Stable"
+
+    StageAll releasesRepo
+    Commit releasesRepo (sprintf "Publish - %s" versionNumber)
+    push releasesRepo
+)
+
+Target "Release" (fun _ ->
+    printfn "Create GitHub Release here..."
+)
+
 Target "Default" DoNothing
 
 "Clean"
@@ -51,7 +113,8 @@ Target "Default" DoNothing
     ==> "Build"
     ==> "Test"
     ==> "Package"
-    ==> "Publish"
+    =?> ("Publish", isAppVeyor)
+    =?> ("Release", isAppVeyor && isStable)
     ==> "Default"
 
 RunParameterTargetOrDefault "target" "Default"
