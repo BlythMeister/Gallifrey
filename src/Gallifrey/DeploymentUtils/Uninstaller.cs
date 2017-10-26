e7c4adaa75e8497583e09706afa93b5fccd9bb4b
Chris Blyth <blythmeister@gmail.com>
2017-10-26 13:14:20 +0100

    Fixes to auto-reinstaller logic

--- src/Gallifrey/DeploymentUtils/Uninstaller.cs
+++ src/Gallifrey/DeploymentUtils/Uninstaller.cs
@@ -1,101 +1,91 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Deployment.Application;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
 using System.Security.Policy;
 using System.Text;
-using Microsoft.Win32;
+using System.Threading;
 
 namespace Gallifrey.DeploymentUtils
 {
     public class Uninstaller
     {
-
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
-
-
-        /// <summary>
-        /// Uninstalls the current ClickOnce app.
-        /// </summary>
         public static void UninstallMe()
         {
-            //System.Diagnostics.Debugger.Break();
-            string publicKeyToken = GetPublicKeyToken();
-            // Find Uninstall string in registry    
-            string DisplayName;
-            string uninstallString = GetUninstallString(publicKeyToken, out DisplayName);
-            string runDLL32 = uninstallString.Substring(0, 12);
-            string args = uninstallString.Substring(13);
-            Process uninstallProcess = Process.Start(runDLL32, args);
-            PushUninstallOKButton(DisplayName);
+            var publicKeyToken = GetPublicKeyToken();
+            var uninstallString = GetUninstallString(publicKeyToken, out var displayName);
+            var runDLL32 = uninstallString.Substring(0, 12);
+            var args = uninstallString.Substring(13);
+            Process.Start(runDLL32, args);
+            PushUninstallOKButton(displayName);
         }
 
-        private static void PushUninstallOKButton(string DisplayName)
+        public static void AutoInstall(string applicationUriString)
         {
-            IntPtr uninstallerWin = FindUninstallerWindow(DisplayName);
-            IntPtr OKButton = FindUninstallerOKButton(uninstallerWin);
+            using (var host = new InPlaceHostingManager(new Uri(applicationUriString), false))
+            {
+                GetApplicationManifest(host);
+                host.AssertApplicationRequirements();
+                DownloadApplication(host);
+            }
+        }
+
+        private static void PushUninstallOKButton(string displayName)
+        {
+            var uninstallerWin = FindUninstallerWindow(displayName);
+            var OKButton = FindUninstallerOKButton(uninstallerWin);
             Win32Utils.DoButtonClick(OKButton);
         }
 
-        private static IntPtr FindUninstallerOKButton(IntPtr UninstallerWindow)
+        private static IntPtr FindUninstallerOKButton(IntPtr uninstallerWindow)
         {
-            Win32Utils w32 = new Win32Utils();
-            IntPtr OKButton = IntPtr.Zero;
+            var w32 = new Win32Utils();
+            var OKButton = IntPtr.Zero;
             while (OKButton == IntPtr.Zero)
             {
-                OKButton = w32.SearchForChildWindow(UninstallerWindow, "&OK");
-                System.Threading.Thread.Sleep(500);
+                OKButton = w32.SearchForChildWindow(uninstallerWindow, "&OK");
+                Thread.Sleep(500);
             }
 
             return OKButton;
         }
 
-        private static IntPtr FindUninstallerWindow(string DisplayName)
+        private static IntPtr FindUninstallerWindow(string displayName)
         {
 
-            Win32Utils w32 = new Win32Utils();
-            IntPtr uninstallerWindow = IntPtr.Zero;
+            var w32 = new Win32Utils();
+            var uninstallerWindow = IntPtr.Zero;
             while (uninstallerWindow == IntPtr.Zero)
             {
-                uninstallerWindow = w32.SearchForTopLevelWindow(DisplayName + " Maintenance");
-                System.Threading.Thread.Sleep(500);
+                uninstallerWindow = w32.SearchForTopLevelWindow(displayName + " Maintenance");
+                Thread.Sleep(500);
             }
 
             return uninstallerWindow;
 
         }
 
-        /// <summary>
-        /// Gets the public key token for the current ClickOnce app.
-        /// </summary>
-        /// <returns></returns>
         private static string GetPublicKeyToken()
         {
-            ApplicationSecurityInfo asi = new ApplicationSecurityInfo(AppDomain.CurrentDomain.ActivationContext);
-            byte[] pk = asi.ApplicationId.PublicKeyToken;
-            StringBuilder pkt = new StringBuilder();
-            for (int i = 0; i < pk.GetLength(0); i++)
+            var asi = new ApplicationSecurityInfo(AppDomain.CurrentDomain.ActivationContext);
+            var pk = asi.ApplicationId.PublicKeyToken;
+            var pkt = new StringBuilder();
+            for (var i = 0; i < pk.GetLength(0); i++)
                 pkt.Append($"{pk[i]:x}");
             return pkt.ToString();
         }
 
-        /// <summary>
-        /// Gets the uninstall string for the current ClickOnce app from the Windows 
-        /// Registry.
-        /// </summary>
-        /// <param name="PublicKeyToken">The public key token of the app.</param>
-        /// <param name="DisplayName"></param>
-        /// <returns>The command line to execute that will uninstall the app.</returns>
+
         private static string GetUninstallString(string PublicKeyToken, out string DisplayName)
         {
             string uninstallString = null;
-            string searchString = "PublicKeyToken=" + PublicKeyToken;
-            RegistryKey uninstallKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall");
-            string[] appKeyNames = uninstallKey.GetSubKeyNames();
+            var searchString = "PublicKeyToken=" + PublicKeyToken;
+            var uninstallKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall");
+            var appKeyNames = uninstallKey.GetSubKeyNames();
             DisplayName = null;
-            foreach (string appKeyName in appKeyNames)
+            foreach (var appKeyName in appKeyNames)
             {
-                RegistryKey appKey = uninstallKey.OpenSubKey(appKeyName);
+                var appKey = uninstallKey.OpenSubKey(appKeyName);
                 uninstallString = (string)appKey.GetValue("UninstallString");
                 DisplayName = (string)appKey.GetValue("DisplayName");
                 appKey.Close();
@@ -106,9 +96,54 @@ namespace Gallifrey.DeploymentUtils
             return uninstallString;
         }
 
-        public static void AutoInstall(string ApplicationUriString)
+        private static void GetApplicationManifest(InPlaceHostingManager host)
         {
-            Process.Start("iexplore.exe", ApplicationUriString);
+            var waitHandle = new AutoResetEvent(false);
+            Exception exception = null;
+
+            host.GetManifestCompleted += (sender, e) =>
+            {
+
+                if (e.Error != null)
+                {
+                    exception = e.Error;
+                }
+
+                waitHandle.Set();
+
+            };
+
+            host.GetManifestAsync();
+
+            waitHandle.WaitOne();
+
+            if (exception != null) throw exception;
+
+        }
+
+        private static void DownloadApplication(InPlaceHostingManager host)
+        {
+
+            var waitHandle = new AutoResetEvent(false);
+            Exception exception = null;
+
+            host.DownloadApplicationCompleted += (sender, e) =>
+            {
+
+                if (e.Error != null)
+                {
+                    exception = e.Error;
+                }
+
+                waitHandle.Set();
+
+            };
+
+            host.DownloadApplicationAsync();
+
+            waitHandle.WaitOne();
+
+            if (exception != null) throw exception;
         }
     }
 }
