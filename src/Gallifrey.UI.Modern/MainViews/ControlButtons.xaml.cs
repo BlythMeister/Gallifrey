6e615a9ee4f0f806d3cc341d048b37dae6cbdfcf
Chris Blyth <blythmeister@gmail.com>
2017-12-05 13:08:21 +0000

    Update internal Gallifrey URLs to be SSL

--- src/Gallifrey.UI.Modern/MainViews/ControlButtons.xaml.cs
+++ src/Gallifrey.UI.Modern/MainViews/ControlButtons.xaml.cs
@@ -288,7 +288,7 @@ namespace Gallifrey.UI.Modern.MainViews
         private void PayPalButton(object sender, RoutedEventArgs e)
         {
             ModelHelpers.Gallifrey.TrackEvent(TrackingType.PayPalClick);
-            Process.Start(new ProcessStartInfo("http://www.gallifreyapp.co.uk/donations.html"));
+            Process.Start(new ProcessStartInfo("https://www.gallifreyapp.co.uk/donations.html"));
         }
 
         private void GitterButton(object sender, RoutedEventArgs e)
